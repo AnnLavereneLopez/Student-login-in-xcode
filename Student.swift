eae5077ec2c61c6d6d580bdcd30adaac9cd3115f
Ann Laverene Lopez <36256249+AnnLavereneLopez@users.noreply.github.com>
2019-07-09 17:52:03 +0530

    Add files via upload

---
+++ Student.swift
@@ -0,0 +1,19 @@
+//
+//  Student.swift
+//  DataLogin
+//
+//  Created by Laverene Lopez on 02/07/19.
+//  Copyright © 2019 Laverene Lopez. All rights reserved.
+//
+
+import UIKit
+
+class Student {
+
+    var name:String?;
+    var rollNo:String?;
+    var subject:String?;
+    var mark:String?;
+    
+    
+}
